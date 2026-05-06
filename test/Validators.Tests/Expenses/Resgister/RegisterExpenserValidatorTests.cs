5bed5e3ba9c70bd137dbead0ccc06b2b3d9ef0e9
Gabriel Martorelli <gabriel.martorelli@hotmail.com>
2026-05-06 15:25:45 -0300

    Add Validators.Tests project with RegisterExpenseValidator test
    Added Validators.Tests project to the solution with xUnit setup and references to application and communication layers. Implemented RegisterExpenserValidatorTests with a unit test for successful validation. Updated solution file to include the new test project.

---
+++ test/Validators.Tests/Expenses/Resgister/RegisterExpenserValidatorTests.cs
@@ -0,0 +1,29 @@
+﻿using CashFlow.Application.UseCases.Expense.Register;
+using CashFlow.Communication.Enums;
+using CashFlow.Communication.Requests;
+
+namespace Validators.Tests.Expenses.Resgister;
+
+public class RegisterExpenserValidatorTests
+{
+    [Fact]
+    public void Success()
+    {
+        //Arrange
+        var validator = new RegisterExpenseValidator();
+        var request = new RequestRegisterExpenseJson
+        {
+            Amount = 100,
+            Date = DateTime.Now.AddDays(-1),
+            Descrition = "Test",
+            PaymentType = PaymentType.CreditCard,
+            Title = "Apple"
+        };
+
+        //Act
+        var result = validator.Validate(request);
+
+        //Assert
+        Assert.True(result.IsValid);
+    }
+}
