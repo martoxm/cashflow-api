52304663aa32dc773e18a91f5612b253aff60f09
Gabriel Martorelli <gabriel.martorelli@hotmail.com>
2026-05-08 19:37:51 -0300

    change name description

--- src/CashFlow.Communication/Requests/RequestRegisterExpenseJson.cs
+++ src/CashFlow.Communication/Requests/RequestRegisterExpenseJson.cs
@@ -5,7 +5,7 @@ namespace CashFlow.Communication.Requests;
 public class RequestRegisterExpenseJson
 {
     public string Title { get; set; } = string.Empty;
-    public string Descrition { get; set; } = string.Empty;
+    public string Description { get; set; } = string.Empty;
     public DateTime Date { get; set; }
     public decimal Amount { get; set; }
     public PaymentType PaymentType { get; set; }
--- test/CommonTestUtilities/Requests/RequestRegisterExpenseJsonBuilder.cs
+++ test/CommonTestUtilities/Requests/RequestRegisterExpenseJsonBuilder.cs
@@ -10,7 +10,7 @@ public class RequestRegisterExpenseJsonBuilder
     {
         return new Faker<RequestRegisterExpenseJson>()
               .RuleFor(r => r.Title, faker => faker.Commerce.ProductName())
-              .RuleFor(r => r.Descrition, faker => faker.Commerce.ProductDescription())
+              .RuleFor(r => r.Description, faker => faker.Commerce.ProductDescription())
               .RuleFor(r => r.Date, faker => faker.Date.Past())
               .RuleFor(r => r.Amount, faker => faker.Random.Decimal(min: 1, max: 1000))
               .RuleFor(r => r.PaymentType, faker => faker.PickRandom<PaymentType>());
