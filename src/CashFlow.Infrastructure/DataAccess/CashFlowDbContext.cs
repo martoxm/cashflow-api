ce7d344102147c6ab87c273ed9046ba276e4cc20
Gabriel Martorelli <gabriel.martorelli@hotmail.com>
2026-05-11 17:23:52 -0300

    Refactor DB config to use connection string from settings
    Update DI to inject IConfiguration and read MySQL connection string from appsettings. Refactor CashFlowDbContext for constructor injection and remove hardcoded config. Update DependencyInjectionExtension for proper DbContext setup.

--- src/CashFlow.Infrastructure/DataAccess/CashFlowDbContext.cs
+++ src/CashFlow.Infrastructure/DataAccess/CashFlowDbContext.cs
@@ -5,15 +5,9 @@ namespace CashFlow.Infrastructure.DataAccess;
 
 internal class CashFlowDbContext : DbContext
 {
-    public DbSet<Expense> Expenses { get; set; }
-
-    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    public CashFlowDbContext(DbContextOptions options) : base(options)
     {
-        var connectionString = "Server=localhost;Database=cashflowdb;Uid=root;Pwd=@Password123;";
-
-        var version = new Version(8, 0, 46);
-        var serverVersion = new MySqlServerVersion(version);
-
-        optionsBuilder.UseMySql(connectionString, serverVersion);
     }
+
+    public DbSet<Expense> Expenses { get; set; }
 }
